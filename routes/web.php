ecc7a2be3cf5b57b8de25bb1137fa85e9c5fe9c6
ibernabel <id.bernabel@gmail.com>
2024-12-14 21:44:06 -0400

    Initial Setup, Laravel Permission and  AdminLTE installed, Initial Roles and permission seeders

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,40 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Spatie\Permission\Models\Role;
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
 Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
+  'auth:sanctum',
+  config('jetstream.auth_session'),
+  'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+
+  Route::get('/dashboard', function () {
+    return view('dashboard');
+  })->name('dashboard');
+
+  Route::get('/users', function () {
+    return 'Vista administracion de usuarios';
+    //return view('users');
+  })->name('users');
+
+  Route::get('/application', function () {
+    return redirect('dashboard');
+  })->name('application');
+
+  Route::get('/application/{id}', function () {
+    return 'Vista administracion de solicitudes';
+    //return view('application');
+  })->name('application');
+
 });
+
+Route::get('/prueba', function () {
+  $admin = Role::find('admin');
+  return 'Pagina de Pruebas ' . $admin;
+  //return view('home');
+})->name('prueba');
