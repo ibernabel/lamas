cfc179c06a3adc275d2a9592f9b97053886cb3a2
ibernabel <id.bernabel@gmail.com>
2025-04-09 19:00:39 -0400

    feat: add reference email, reference since date, and active status to customer references

---
+++ database/migrations/2025_04_09_185540_add_fields_to_customer_references_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('customer_references', function (Blueprint $table) {
+
+            $table->string('reference_email')->nullable()->after('phone_number')->comment('Email address of the reference');
+            $table->date('reference_since')->nullable()->after('relationship')->comment('Date since the customer has known the reference');
+            $table->boolean('is_active')->default(true)->after('reference_since')->comment('Indicates if the reference is active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('customer_references', function (Blueprint $table) {
+            $table->dropColumn('reference_email');
+            $table->dropColumn('reference_since');
+            $table->dropColumn('is_active');
+        });
+    }
+};
