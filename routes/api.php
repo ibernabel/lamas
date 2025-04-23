5d1eae5246eddc1401884a53793891287d024c9f
ibernabel <id.bernabel@gmail.com>
2025-04-23 15:33:03 -0400

    feat: Implement authentication endpoints for login, logout, and token status

--- routes/api.php
+++ routes/api.php
@@ -2,28 +2,27 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\LoanApplicationController as ApiLoanApplicationController;
 use App\Http\Controllers\Api\V1\CustomerController; // Add this line
 
 // Group related routes with common middleware
 Route::prefix('v1')->middleware([
-//  'auth:sanctum'
+    //  'auth:sanctum'
 ])->group(function () {
-  // API Resource routes for loan applications
-  // Use apiResource for standard API methods
-  //->only(['index', 'show', 'store', 'update', 'destroy']);
-  Route::apiResource('loan-applications', ApiLoanApplicationController::class);
+    // API Resource routes for loan applications
+    // Use apiResource for standard API methods
+    //->only(['index', 'show', 'store', 'update', 'destroy']);
+    Route::apiResource('loan-applications', ApiLoanApplicationController::class);
 
-  // Route to check if customer NID exists
-  Route::get('/customers/nid/{nid}/exists', [CustomerController::class, 'checkNidExists'])->name('api.v1.customers.nid.exists');
-});
-
-// Authentication routes
-Route::post('/login', [AuthController::class, 'login']);
-
-// Routes requiring authentication
-Route::middleware('auth:sanctum')->group(function () {
+    // Route to check if customer NID exists
+    Route::get('/customers/nid/{nid}/exists', [CustomerController::class, 'checkNidExists'])->name('api.v1.customers.nid.exists');
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/tokenStatus', [AuthController::class, 'tokenStatus']); // Updated to use tokenStatus method
+});
+
+// Group related routes without Auth middleware
+Route::prefix('v1')->group(function () {
+
+    Route::post('/login', [AuthController::class, 'login']);
 });
