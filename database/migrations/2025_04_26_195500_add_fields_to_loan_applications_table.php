e8f279ba90798347665e6eec31245c7850d9b10e
ibernabel <id.bernabel@gmail.com>
2025-04-26 20:07:27 -0400

    feat: Add user_id field to loan_applications table for user association

---
+++ database/migrations/2025_04_26_195500_add_fields_to_loan_applications_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('loan_applications', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('user_id')->nullable()->after('customer_id')->comment('ID of the user associated with the loan application');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('loan_applications', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+};
