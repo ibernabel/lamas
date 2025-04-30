929935798a7d39c36084fe6ad2dd2edcac352491
ibernabel <id.bernabel@gmail.com>
2025-04-30 11:36:27 -0400

    fix: Correct model name from Costumer to Customer and update policy methods for proper access control

--- app/Policies/CostumerPolicy.php
+++ app/Policies/CostumerPolicy.php
@@ -2,11 +2,11 @@
 
 namespace App\Policies;
 
-use App\Models\Costumer;
+use App\Models\Customer;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class CostumerPolicy
+class CustomerPolicy
 {
     /**
      * Determine whether the user can view any models.
@@ -19,9 +19,9 @@ class CostumerPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Costumer $costumer): bool
+    public function view(User $user, Customer $customer): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -29,29 +29,29 @@ class CostumerPolicy
      */
     public function create(User $user): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Costumer $costumer): bool
+    public function update(User $user, Customer $customer): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Costumer $costumer): bool
+    public function delete(User $user, Customer $customer): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Costumer $costumer): bool
+    public function restore(User $user, Customer $customer): bool
     {
         return false;
     }
@@ -59,7 +59,7 @@ class CostumerPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Costumer $costumer): bool
+    public function forceDelete(User $user, Customer $customer): bool
     {
         return false;
     }
