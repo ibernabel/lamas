72d161dda89cd84b5f85854a85f99cc3b1ee8795
ibernabel <id.bernabel@gmail.com>
2025-05-01 12:11:23 -0400

    feat: Add UserSeeder to create initial admin user with roles

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create roles
+        User::create([
+            'name' => env('MAIN_ADMIN_NAME'),
+            'email' => env('MAIN_ADMIN_EMAIL'),
+            'password' => bcrypt(env('MAIN_ADMIN_PASSWORD')),
+            'is_approved' => true,
+            'is_active' => true,
+            'created_at' => now(),
+            'updated_at' => now()
+        ])->assignRole('admin');
+
+        //User::factory(10)->withPersonalTeam()->create();
+    }
+}
